7e8d79eb3d484c66d78f6ac615ba724c4aae3ea1
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2025-02-14 07:19:27 -0300

    Atividade N°4 from Python Brasil

---
+++ Atividades_Python_Brasil_to_Java/Atividade_3/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //3. Faça um Programa que peça dois números e imprima a soma.
+        Scanner reader = new Scanner(System.in);
+
+        double n1 = 0.0;
+        double n2 = 0.0;
+        double soma = 0.0;
+
+        System.out.print("Informe o primeiro nnúmero: ");
+        n1 = reader.nextDouble();
+
+        System.out.print("Informe o segundo número: ");
+        n2 = reader.nextDouble();
+
+        soma = n1 +  n2;
+
+        System.out.printf("A soma dos dosi números é de: %.2f", soma);
+    }
+}
