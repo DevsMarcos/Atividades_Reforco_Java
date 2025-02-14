ac5adee4c5b2c692a166f83d3cb1290257d7e91c
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2025-02-14 07:07:41 -0300

    Atividade N°2 from Python Brasil

---
+++ Atividades_Python_Brasil_to_Java/Ativade_2/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*2. Faça um Programa que peça um número e então mostre a mensagem
+        O número informado foi [número].*/
+
+        Scanner reader =  new Scanner(System.in);
+        double numero = 0.0;
+
+        System.out.print("Informe um número qualquer: ");
+        numero = reader.nextDouble();
+
+        System.out.printf("O número informado foi: %.2f", numero);
+    }
+}
